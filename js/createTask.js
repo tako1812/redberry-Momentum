5f2401a901323b047fbe4bae9e7d6369ee7174e0
Tamar Robakidze <troba12@freeuni.edu.ge>
2025-03-18 19:58:53 +0400

    feat - add functionality on loading employees after selecting departments

--- js/createTask.js
+++ js/createTask.js
@@ -1,5 +1,7 @@
 "use strict";
-const prioritiesContainer = document.querySelector(".priorities-continer");
+const token = "9e73c158-43ef-4fd6-9f0e-70385f360191";
+const prioritiesContainer = document.querySelector(".dropdown-priorities");
+const ddd= document.querySelector(".default");
 const renderPriorities = async function () {
     prioritiesContainer.innerHTML = "";
     const res = await fetch(
@@ -10,16 +12,29 @@ const renderPriorities = async function () {
   
     datas.map(data => {
         const html = `
-        <option><img src="https://momentum.redberryinternship.ge/storage/priority-icons/Low.svg"/>nnn </option>
-        <option value="${data.name}" style="background-image:url(${data.icon})";>${data.name}</option>
-        <option style="background-image:url(${data.icon});">male</option>
+        <a class="img-icon-container">
+          <img id="dropdown-icon" src="${data.icon}" alt="priority icon"/>
+          <p class="category">${data.name}</p>
+        </a>
         `;
 
         prioritiesContainer.insertAdjacentHTML("afterbegin", html);
     });
+    
+    /*const [defaultValue] = datas.filter((data) => data.name === "საშუალო"); 
+    console.log(defaultValue);
 
+    ddd.innerHTML = "";
+    const markup = ` 
+        <img class="icon-selected"src="${defaultValue.icon}" alt="priority icon"/>
+        <p class="category-selected">${defaultValue.name}</p>
+       `;
+      
+    ddd.insertAdjacentHTML("afterbegin", markup);*/
+  
   };
   renderPriorities();
+  
 ////
 ////
 const statusesContainer = document.querySelector(".statuses-container");
@@ -41,51 +56,160 @@ const statusesContainer = document.querySelector(".statuses-container");
     });
   };
   renderStatuses();
-  /*
-  const departmentsContainer = document.querySelector(".department-container");
+  /////////////////////////////////////////
+  ////////////////////////////////////////
   
-  const renderDepartments = async function () {
-    departmentsContainer .innerHTML = "";
+  const departmentContainer = document.querySelector(".department-container");
+
+  let dataDepartments;
+  let dataEmployees;
+  
+  const renderDepartment = async function () {
+    departmentContainer .innerHTML = "";
     const res = await fetch(
       "https://momentum.redberryinternship.ge/api/departments"
     );
     const datas = await res.json();
     console.log(datas);
-    departments = datas;
-  
+    dataDepartments = datas; 
+    
+
     datas.map(data => {
         const html = `
-        <option value="${data.name}">${data.name}</option>
+        <option class="department" value="${data.id}">${data.name}</option>
         `;
 
-        departmentsContainer .insertAdjacentHTML("afterbegin", html);
+        departmentContainer.insertAdjacentHTML("afterbegin", html);
     });
+
+    const resp = await fetch("https://momentum.redberryinternship.ge/api/employees",
+      {
+          method: "GET",
+          headers: {
+          Authorization: `Bearer ${token}`,
+          accept: "application/json",
+          },
+      }
+      );
+      const dataEmp = await resp.json();
+      console.log(dataEmp);
+      dataEmployees = dataEmp;
+
   };
-  renderDepartments();
-*/
+  renderDepartment();
+
+
+  const selectedDepartment = document.querySelector(".departments-dropdown");
+  selectedDepartment.addEventListener("change", function(e) {
+    
+
+    const departmentEmployee = dataEmployees.filter(employee => {
+      console.log(employee);
+      return employee.department.id === +selectedDepartment.value;
+
+    });
+    dropdownContentEmployees.innerHTML = "";
+    departmentEmployee.map(employee => {
+      const html = `
+        <a class="employee-info-container">
+          <img src="${employee.avatar}" alt="priority icon"/>
+          <p class="category">${employee.name} ${employee.surname}</p>
+        </a>
+      `
+      dropdownContentEmployees.insertAdjacentHTML("afterbegin", html);
+    });
+
+  })
 ////////////////////////////////
 ///////////////////////////////
-//////////////////////////////
+/*
+const renderEmployees = async function () {
+  dropdownContentEmployee.innerHTML = "";
+  
+  const res = await fetch("https://momentum.redberryinternship.ge/api/employees",
+      {
+          method: "GET",
+          headers: {
+          Authorization: `Bearer ${token}`,
+          accept: "application/json",
+          },
+      }
+      );
+      const datas = await res.json();
+      console.log(datas);
+
+      datas.map(data => {
+          const html = `
+           <a class="img-icon-container">
+             <img src="https://media.geeksforgeeks.org/wp-content/uploads/20200630132504/uflag.jpg" alt="priority icon"/>
+            <p class="category">დაბალი</p>
+          </a>
+      `;
+
+      dropdownContentEmployee.insertAdjacentHTML("afterbegin", html);
+  });
+
+};
+renderFilterEmployees();*/
+
+
+
+
+const dropdownContentEmployees = document.querySelector(".dropdown-content-employees");
+const employeeImage =document.querySelector(".info-employee-img");
+const employeeFullname = document.querySelector(".info-employee-fullName");
+
+//////////////////////////////////////////
+/////////////////////////////////////////
 // customize dropdown functionality
 ///
 
-const dropdownBtn = document.querySelector(".dropdown-btn");
-const dropdownContent = document.querySelector(".dropdown-content");
-const iconSelected = document.querySelector(".icon-selected");
-const categorySelected = document.querySelector(".category-selected");
+const dropdownBtn = document.querySelector(".dropdown-btn-prior");
+const dropdownContentPriorities = document.querySelector(".dropdown-content-priorities");
+const iconSelected = document.querySelector(".icon-selected-prior");
+const categorySelected = document.querySelector(".category-selected-prior");
 dropdownBtn.addEventListener("click", function(e) {
   e.preventDefault();
-  dropdownContent.classList.toggle("hidden");
+  dropdownContentPriorities.classList.toggle("hidden");
 });
 
 const dropdownBtnFunctionality = function(e) {
   const clicked = e.target.closest(this);
+  console.log(clicked);
   const icon = clicked.querySelector("img");
   const iconSource = icon.getAttribute("src");
   
-  categorySelected.textContent = clicked.textContent;
-  iconSelected.setAttribute("src", iconSource);
-  dropdownContent.classList.add("hidden");
+  if(clicked.classList.contains("img-icon-container")){
+    categorySelected.textContent = clicked.textContent;
+    iconSelected.setAttribute("src", iconSource);
+    dropdownContentPriorities.classList.add("hidden");
+  }
+  if(clicked.classList.contains("employee-info-container")){
+    employeeFullname.textContent = clicked.textContent;
+    employeeImage.setAttribute("src", iconSource);
+    dropdownContentEmployees.classList.add("hidden");
+  }
 
-}
-dropdownContent.addEventListener("click", dropdownBtnFunctionality.bind(".img-icon-container"));
+} 
+
+
+dropdownContentPriorities.addEventListener("click", dropdownBtnFunctionality.bind(".img-icon-container"));
+dropdownContentEmployees.addEventListener("click", dropdownBtnFunctionality.bind(".employee-info-container"));
+
+//////////////////////////////////////////
+/////////////////////////////////////////
+/////////////////////////////////////////
+/////////////////////////////////////
+//dropdown-btn-employee
+const dropdownBtnEmployee = document.querySelector(".dropdown-btn-employee");
+//const dropdownContentEmployee = document.querySelector(".dropdown-content-employees");
+const iconEmployee = document.querySelector(".icon-selected-emoloyee");
+const categoryEmployee = document.querySelector(".category-selected-employee");
+const employeeInfoContainer = document.querySelector(".employee-info-container");
+
+
+//employees-container
+dropdownBtnEmployee.addEventListener("click", function(e) {
+  e.preventDefault();
+  dropdownContentEmployees.classList.toggle("hidden");
+});
